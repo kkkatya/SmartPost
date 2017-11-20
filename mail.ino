ba8fc7ab9e6938439130f5e50c8037919da7d0ef
Katya <Katya>
2017-11-20 23:35:43 +0100

    Added sensors

--- mail.ino
+++ mail.ino
@@ -6,3 +6,37 @@ void loop() {
     Particle.publish("mail", PRIVATE);
     delay(1000000);
 }
+
+int led = 11;
+int sensor = A0;
+int button = 10;
+bool turnOn = false;
+bool mailSent = false;
+
+void setup()
+{
+  pinMode(led, OUTPUT);
+  pinMode(sensor, INPUT);
+  pinMode(button, INPUT);
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  //Serial.println(analogRead(sensor));
+  //Serial.print("button ");
+  //Serial.println(button);
+  if(analogRead(sensor)>0) turnOn = true;
+  if(digitalRead(button)==HIGH)turnOn = false;
+  if(turnOn){
+  	digitalWrite(led, HIGH);
+  	if(!mailSent){
+  		Particle.publish("mail", PRIVATE);
+  		mailSent = true;
+  	}
+  } 
+  else{
+  	digitalWrite(led, LOW);
+  	mailSent = false;
+  } 
+}
